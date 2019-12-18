696f49da51d816c0f2aa05138c2cbac8cee31d07
Moritz Migge <moritz.migge@web.de>
2019-12-18 12:26:20 +0100

    Update Live.js: ini  connect to Logger

--- client_mobile/src/Live/Live.js
+++ client_mobile/src/Live/Live.js
@@ -6,63 +6,88 @@ import {Container,Row,Col,Card,Table} from 'react-bootstrap'
 var mqtt = require('mqtt')
 var client
 
-class Live extends  Component{
-    constructor(props){
+class Live extends Component {
+    constructor(props) {
         super(props);
         this.state = {
-            username:'erictg96@googlemail.com',
-            password:'9157fbb4',
-            connected:false
+            username: 'erictg96@googlemail.com',
+            password: '9157fbb4',
+            connected: false
 
         }
         this.connectMQTT = this.connectMQTT.bind(this);
         this.disconnectMQTT = this.disconnectMQTT.bind(this);
         this.submit = this.submit.bind(this);
+        this.connectToLogger();
     }
 
-    connectMQTT(){
+    connectToLogger() {
+        const url = 'http://192.168.3.30';
+
+        fetch(url).then(res => {
+                console.log(res);
+                console.log(res.text());
+            })
+            .catch(err => {
+                console.log('err: ', err)
+            });
+
+    }
+
+    connectMQTT() {
         console.log("connectMQTT");
 
         // Creation of client object with the username and password supplied by mqtt.dioty.co
-        client = mqtt.connect("mqtt://mqtt.dioty.co:8080",{
-            username:this.state.username,
-            password:this.state.password
+            client = mqtt.connect("mqtt://mqtt.dioty.co:8080", {
+                username: this.state.username,
+                password: this.state.password
         })
         var that = this;
         // On connect handler for mqtt, sets state and gives some logs
-        client.on('connect', function () {
-            client.subscribe(["/"+that.state.username+"/time","/"+that.state.username+"/temperature","/"+that.state.username+"/pm10","/"+that.state.username+"/humi"], function (err,granted) {
+            client.on('connect', function() {
+                client.subscribe(["/" + that.state.username + "/time", "/" + that.state.username + "/temperature", "/" + that.state.username + "/pm10", "/" + that.state.username + "/humi"], function(err, granted) {
              if (!err) {
-                console.log("Client Subscribe:","Succesfully connected to the given topics!")
-                that.setState({connected:true})
+                        console.log("Client Subscribe:", "Succesfully connected to the given topics!")
+                        that.setState({
+                            connected: true
+                        })
                 console.log("Done!Showing values(if there are any)now!")
-            }
-            else{
-                console.log("Error found when subscribing:",err.message)
+                    } else {
+                        console.log("Error found when subscribing:", err.message)
             }
             })
         })
         
         // if a message of the subscribed topics come in do the following
-        client.on('message',function(topic,message){
+            client.on('message', function(topic, message) {
             console.log(message);
             let value = message.toString();
             console.log(topic);
-            if(topic==='/erictg96@googlemail.com/temperature')that.setState({lastMessageTemp:value})
-            if(topic==='/erictg96@googlemail.com/pm10')that.setState({lastMessagePm10:value})
-            if(topic==='/erictg96@googlemail.com/humi')that.setState({lastMessageHumi:value})
-            if(topic==='/erictg96@googlemail.com/time')that.setState({lastMessageTime:value})
+                if (topic === '/erictg96@googlemail.com/temperature') that.setState({
+                    lastMessageTemp: value
+                })
+                if (topic === '/erictg96@googlemail.com/pm10') that.setState({
+                    lastMessagePm10: value
+                })
+                if (topic === '/erictg96@googlemail.com/humi') that.setState({
+                    lastMessageHumi: value
+                })
+                if (topic === '/erictg96@googlemail.com/time') that.setState({
+                    lastMessageTime: value
+                })
 
             console.log(value);
         })
     }
     // simple disconnect handler, stes state of connected variable
-    disconnectMQTT(){
+    disconnectMQTT() {
         console.log("Disconnecting from MQTT now")
-        this.setState({connected:false})
+        this.setState({
+            connected: false
+        })
         client.end()
     }
-    submit(){
+    submit() {
         console.log("submit");
     }
     render() {
