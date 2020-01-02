6e2a188fb07bb02981fe6c2b02de4c91f345048b
NJaku01 <nick1212@gmx.de>
2020-01-02 16:24:50 +0100

    select and show sample data

--- client_stationary/src/Explore/Explore.js
+++ client_stationary/src/Explore/Explore.js
@@ -1,110 +1,194 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { TextField, MenuItem } from '@material-ui/core'
 import OwnMap from '../Map/OwnMap'
-import {Container,Row,Col,Card,Table,Button} from 'react-bootstrap'
+import { Container, Row, Col, Card, Table, Button } from 'react-bootstrap'
 
-class Explore extends  Component{
-    constructor(props){
+
+var exampleData = {
+    data: [
+        {
+            date: "15-11-2019",
+            data: 0,
+            geojson: {
+                "type": "FeatureCollection",
+                "features": [
+                    {
+                        "type": "Feature",
+                        "properties": {},
+                        "geometry": {
+                            "type": "LineString",
+                            "coordinates": [
+                                [
+                                    7.60528564453125,
+                                    51.97155731422007
+                                ],
+                                [
+                                    7.605628967285155,
+                                    51.964577109947506
+                                ],
+                                [
+                                    7.6197052001953125,
+                                    51.95759581845331
+                                ],
+                                [
+                                    7.632408142089844,
+                                    51.96119237712624
+                                ],
+                                [
+                                    7.634468078613281,
+                                    51.96732701717229
+                                ],
+                                [
+                                    7.622451782226563,
+                                    51.97134580885172
+                                ]
+                            ]
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            date: "24-11-2019",
+            data: 1, 
+            geojson: {
+                "type": "FeatureCollection",
+                "features": [
+                    {
+                        "type": "Feature",
+                        "properties": {},
+                        "geometry": {
+                            "type": "LineString",
+                            "coordinates": [
+                                [
+                                    7.6032257080078125,
+                                    51.9599230364245
+                                ],
+                                [
+                                    7.623481750488281,
+                                    51.96690396550418
+                                ],
+                                [
+                                    7.615585327148437,
+                                    51.94638116576159
+                                ]
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
+    ]
+}
+
+class Explore extends Component {
+    constructor(props) {
         super(props);
-        this.state={date: "12-11-2019"}
+        this.state = { date: "null", route: null }
         this.downloadSelectedRoute = this.downloadSelectedRoute.bind(this)
     }
 
 
-    dates = [
-    {
-        value: '15-11-2019',
-        label: '15. November 2019',
-    },
-    {
-        value: '14-11-2019',
-        label: '14. November 2019',
-    },
-    {
-        value: '13-11-2019',
-        label: '13. November 2019',
-    },
-    {
-        value: '12-11-2019',
-        label: '12. November 2019',
-    },
-];
+    dates = [{value: "null", label: "dd-mm-yyyy"}];
 
-    handlechange = (e) =>{
-        this.setState({date: e.target.value})
+    componentWillMount(){
+        for (var date of exampleData.data){
+            this.dates.push({value: date.date, label: date.date})
+        }
+        console.log(this.dates)
     }
 
-    downloadSelectedRoute(){
+    handlechange = (e) => {
+
+        const value = e.target.value
+        let route;
+        if (value == null){
+            route= null
+        }
+        else{
+        for (var data of exampleData.data){
+            if(data.date=== value)
+            {
+                route= data
+            }
+        }
+    }
+        this.setState({ date: value, route : route })
+
+    }
+
+    downloadSelectedRoute() {
         console.log("downloadSelectedRoute");
     }
     render() {
         return (
             <Container fluid>
                 <div>
-                    <OwnMap/>
+                    <OwnMap route={this.state.route} />
                 </div>
                 <Row>
-                <Col md={8}>
-                    <Card style={{'margin-top':'5px'}}>
-                        <Card.Body>
-                        <Card.Title>Hier kannst du Details zu der ausgewählten Route betrachten.</Card.Title>
-                        <Table striped bordered hover>
-                        <thead>
-                            <tr>
-                            <th>#</th>
-                            <th>Sensor</th>
-                            <th>Messwert</th>
-                            <th>Zeit</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                            <td>1</td>
-                            <td>Temperatur</td>
-                            <td>33°C</td>
-                            <td>2019-12-09T12:39:00</td>
-                            </tr>
-                            <tr>
-                            <td>2</td>
-                            <td>PM10</td>
-                            <td>22.45µg/m³</td>
-                            <td>2019-12-09T12:39:00</td>
-                            </tr>
-                            <tr>
-                            <td>3</td>
-                            <td>rel. Luftfeuchtigkeit</td>
-                            <td>78%</td>
-                            <td>2019-12-09T12:39:00</td>
-                            </tr>
-                        </tbody>
-                        </Table>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col md={4}>
-                <Card style={{'margin-top':'5px'}}>
-                    <Card.Body>
-                        <Card.Title>Wähle eine vorherige Route aus um sie auf der Karte anzuzeigen.</Card.Title>
-                <TextField
-                    id="standard-select-date"
-                    select
-                    label=""
-                    value={this.state.date}
-                    onChange={this.handlechange.bind(this)} 
-                    margin="normal"
-                    variant="outlined"
-                >
-                    {this.dates.map(option => (
-                        <MenuItem key={option.value} value={option.value}>
-                            {option.label}
-                        </MenuItem>
-                    ))}
-                </TextField>
-                <br></br>
-                <Button onClick={this.downloadSelectedRoute} variant="primary">Diese Route herunterladen</Button>
-                </Card.Body>
-                </Card>
-                </Col>
+                    <Col md={8}>
+                        <Card style={{ 'margin-top': '5px' }}>
+                            <Card.Body>
+                                <Card.Title>Hier kannst du Details zu der ausgewählten Route betrachten.</Card.Title>
+                                <Table striped bordered hover>
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Sensor</th>
+                                            <th>Messwert</th>
+                                            <th>Zeit</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>Temperatur</td>
+                                            <td>33°C</td>
+                                            <td>2019-12-09T12:39:00</td>
+                                        </tr>
+                                        <tr>
+                                            <td>2</td>
+                                            <td>PM10</td>
+                                            <td>22.45µg/m³</td>
+                                            <td>2019-12-09T12:39:00</td>
+                                        </tr>
+                                        <tr>
+                                            <td>3</td>
+                                            <td>rel. Luftfeuchtigkeit</td>
+                                            <td>78%</td>
+                                            <td>2019-12-09T12:39:00</td>
+                                        </tr>
+                                    </tbody>
+                                </Table>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col md={4}>
+                        <Card style={{ 'margin-top': '5px' }}>
+                            <Card.Body>
+                                <Card.Title>Wähle eine vorherige Route aus um sie auf der Karte anzuzeigen.</Card.Title>
+                                <TextField
+                                    id="standard-select-date"
+                                    select
+                                    label=""
+                                    value={this.state.date}
+                                    onChange={this.handlechange.bind(this)}
+                                    margin="normal"
+                                    variant="outlined"
+                                    placholder="dd-mm-yyyy"
+                                >
+                                    {this.dates.map(option => (
+                                        <MenuItem key={option.value} value={option.value}>
+                                            {option.label}
+                                        </MenuItem>
+                                    ))}
+                                </TextField>
+                                <br></br>
+                                <Button onClick={this.downloadSelectedRoute} variant="primary">Diese Route herunterladen</Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
                 </Row>
             </Container>
 
--- client_stationary/src/Map/OwnMap.js
+++ client_stationary/src/Map/OwnMap.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { Map, TileLayer, FeatureGroup,Marker,Popup } from 'react-leaflet'
 
+import L from 'leaflet'
+
+let firstTime = true
+
+export let ref;
 
 class OwnMap extends React.Component {
     constructor(props) {
@@ -11,21 +16,58 @@ class OwnMap extends React.Component {
     };
 
 
+    _onFeatureGroupReady = (ref) => {
+
+        if (!firstTime) {
+            return;
+        }
+        this.FG = ref;
+        let GeoJSON = this.getGeoJson()
+        let leafletGeoJSON = new L.GeoJSON(GeoJSON);
+        let leafletFG = this.FG.leafletElement;
+        leafletGeoJSON.eachLayer(layer => leafletFG.addLayer(layer));
+        firstTime = false;
+    }
+
+    componentDidUpdate(prevProps) {
+        // Typical usage (don't forget to compare props):
+        console.log(prevProps)
+        console.log(this.props)
+         {
+            console.log("updated")
+            this.FG = ref;
+            let GeoJSON = this.getGeoJson()
+            let leafletGeoJSON = new L.GeoJSON(GeoJSON);
+            let leafletFG = this.FG.leafletElement;
+            leafletFG.clearLayers()
+            leafletGeoJSON.eachLayer(layer => leafletFG.addLayer(layer));
+        }
+      }
+
+
+
+    getGeoJson = () => {
+        if(this.props.route){
+            return this.props.route.geojson
+        }
+        else{ return null}
+    }
+
+
+
+
 
     render() {
         const position = [51.9688129,7.5922197];
 
         return (
-            <Map style={{ height: "50vh" }} center={position} zoom={16} ref="map">
+            <Map style={{ height: "50vh" }} center={position} zoom={11} ref="map">
                 <TileLayer
                     attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position = {position}>
-                    <Popup open>Das Fahrrad steht hier</Popup>
-                </Marker>
-
-
+                <FeatureGroup ref={(reactFGref) => { this._onFeatureGroupReady(reactFGref); ref = reactFGref }}>
+                </FeatureGroup>
             </Map>
             );
     }
