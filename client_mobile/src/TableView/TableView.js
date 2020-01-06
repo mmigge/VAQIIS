c0f03911c665fc02ffb29833f30efc92257f6982
Thiemann96 <e_thie10@uni-muenster.de>
2020-01-06 16:22:42 +0100

    implement table view logic, files not from server

--- client_mobile/src/TableView/TableView.js
+++ client_mobile/src/TableView/TableView.js
@@ -3,12 +3,16 @@ import { TextField, MenuItem } from '@material-ui/core'
 import OwnMap from '../Map/OwnMap'
 import {Container,Row,Col,Card,Table,Button} from 'react-bootstrap'
 import des from './des.json'
+import fastTable from './fastTable.json'
+import fastTableValues from './fastTableValues'
 
 class TableView extends  Component{
     constructor(props){
         super(props);
         this.state={
-            data:des
+            data:des,
+            fastTable:fastTable,
+            fastTableValues:fastTableValues
         }
     }
 
@@ -16,9 +20,7 @@ class TableView extends  Component{
         // pose request to get all fields from the fastdata table 
         let url = 'http://128.176.146.233:3134/logger/command=browseSymbols&format=json'
         var keyNames = Object.keys(this.state.data.symbols)
-        console.log(keyNames)
 
-         console.log(des)
         // fetch(url)
         // .then((response)=>{
         //     console.log(response)
@@ -40,9 +42,11 @@ class TableView extends  Component{
     }
 
     render() {
+        console.log(this.state.fastTableValues)
         return (
             <Container fluid>
                 <Table striped bordered hover>
+                    <thead>
                     <tr>
                         <th>Name</th>
                         <th>uri</th>
@@ -51,6 +55,8 @@ class TableView extends  Component{
                         <th>is_read_only</th>
                         <th>can_expand</th>
                     </tr>
+                    </thead>
+                    <tbody>
                 {this.state.data.symbols.map((field)=>(
                     <tr>
                         <td>{field.name}</td>
@@ -61,6 +67,56 @@ class TableView extends  Component{
                         <td>{field.can_expand.toString()}</td>
                     </tr>
                     ))}
+                    </tbody>
+                </Table>
+                <hr></hr>
+                <Table striped bordered hover>
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>uri</th>
+                        <th>type</th>
+                        <th>is_enabled</th>
+                        <th>is_read_only</th>
+                        <th>can_expand</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                {this.state.fastTable.symbols.map((field)=>(
+                <tr>
+                    <td>{field.name}</td>
+                    <td>{field.uri}</td>
+                    <td>{field.type}</td>
+                    <td>{field.is_enabled.toString()}</td>
+                    <td>{field.is_read_only.toString()}</td>
+                    <td>{field.can_expand.toString()}</td>
+                </tr>
+                ))}
+                </tbody>
+                </Table>
+                <Table striped bordered hover>
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Type</th>
+                        <th>Units</th>
+                        <th>Process</th>
+                        <th>Settable</th>
+                        <th>Value</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.state.fastTableValues.head.fields.map((field,index)=>(
+                     <tr>
+                     <td>{field.name}</td>   
+                     <td>{field.type}</td>   
+                     <td>{field.process}</td>   
+                     <td>{field.settable}</td>   
+                     <td>{field.name}</td>
+                     <td>{this.state.fastTableValues.data[0].vals[index]}</td>
+                     </tr>   
+                     ))}
+                     </tbody>
                 </Table>
             </Container>
 
