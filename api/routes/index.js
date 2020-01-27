aa4a7dafbb171f0200a867f510fce84c6d1847e9
nick1 <n_jaku01@wwu.de>
2020-01-27 11:58:11 +0100

    productgion build

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var path = require('path');
+
+router.use(express.static(path.join(__dirname, '../../client_stationary/build')));
+router.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../../client_stationary/build', 'index.html'));
 });
 
 module.exports = router;
